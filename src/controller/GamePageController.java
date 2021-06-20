ba25076d7275e3023dd8194cab4a83346f35f9f7
Lenovo <mamiry_1379@yahoo.com>
2021-06-20 15:52:44 +0430

    git commit

--- src/controller/GamePageController.java
+++ src/controller/GamePageController.java
@@ -1,6 +1,5 @@
 package controller;
 
 public class GamePageController {
-String name;
-String name3;
+
 }
---
+++ src/model/Cell.java
@@ -0,0 +1,45 @@
+package model;
+
+import javafx.scene.control.Button;
+
+public class Cell extends Button {
+    private boolean isChoosed;
+    private boolean isBlue;
+    private int x,y;
+
+
+
+    public Cell(int x, int y) {
+        super(" ");
+        this.x = x;
+        this.y = y;
+
+        isBlue=false;
+        isChoosed=false;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isChoosed() {
+        return isChoosed;
+    }
+
+    public void setChoosed(boolean choosed) {
+        isChoosed = choosed;
+    }
+
+    public boolean isBlue() {
+        return isBlue;
+    }
+
+    public void setBlue(boolean blue) {
+        isBlue = blue;
+    }
+}
