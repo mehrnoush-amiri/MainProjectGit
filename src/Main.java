747612341ab01cfd3613e8375c23fe6acdd2ac9e
reihanehrashidi <reihanehrashidi2181@gmail.com>
2021-06-20 18:21:11 +0430

    git commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader ( this.getClass ().getResource ("view/GamePage.fxml") );
+        loader.load ();
+        primaryStage.setScene (new Scene (loader.getRoot ()));
+        primaryStage.show ();
+    }
 }
--- src/controller/GamePageController.java
+++ src/controller/GamePageController.java
@@ -1,5 +1,42 @@
 package controller;
 
-public class GamePageController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GamePageController implements Initializable {
+
+
+    @FXML
+    private TableColumn<?, ?> blueTAB;
+
+    @FXML
+    private TableColumn<?, ?> redTAB;
+
+    @FXML
+    private Label blueLBL;
+
+    @FXML
+    private Label redLBL;
+
+    @FXML
+    private TextField blueField;
+
+    @FXML
+    private TextField redField;
+
+    @FXML
+    private Hyperlink hyperlink;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
