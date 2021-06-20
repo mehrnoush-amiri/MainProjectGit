1f895813fd0577c7b6713ac644ea349365b7d6c6
reihanehrashidi <reihanehrashidi2181@gmail.com>
2021-06-20 22:27:04 +0430

    git commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import com.sun.javafx.tk.ScreenConfigurationAccessor;
+
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
